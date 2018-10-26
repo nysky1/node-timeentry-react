6aa3da6492eb0e32aa9bd6fc16ee26c8e08ec97c
J. Bishop <jbishop@Js-MacBook-Pro.local>
2018-10-25 20:30:58 -0600

    Already have acct link

--- src/components/register.js
+++ src/components/register.js
@@ -49,7 +49,7 @@ export class Register extends React.Component {
                             </fieldset>
                         </form>
                         <div className="lnkBack">
-                            <Link to="/login" aria-label="Click to sign in">Already have an account?</Link>
+                            <Link to="/login">Already have an account?</Link>
                         </div>
                     </div>
                 </div>
