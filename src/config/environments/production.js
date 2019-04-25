11045025302e5318f8970b6ef1ee3fe94b529ccd
J. Bishop <jbishop@Js-MacBook-Pro.local>
2019-04-25 12:09:10 -0600

    no redux

--- src/config/environments/production.js
+++ src/config/environments/production.js
@@ -3,6 +3,6 @@ import defaultConfig from './default';
 export default {
   ...defaultConfig,
   CONFIG_NAME: 'Production Config',
-  SHOW_REDUX_DEV_TOOLS: true,
+  SHOW_REDUX_DEV_TOOLS: false,
   MAIN_APP_AUTHORITY: 'https://stark-reef-77498.herokuapp.com/api'
 };
